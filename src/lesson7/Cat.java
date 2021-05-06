20d090a680d181c73d37f8e4498afd52608ceb0b
Vlad Ivanov <ivanovvlad.261996@mail.ru>
2021-05-06 03:45:07 +0300

    Home work 7

---
+++ src/lesson7/Cat.java
@@ -0,0 +1,34 @@
+package lesson7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean isFull;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.isFull = false;
+    }
+
+    public void eat(Plate p) {
+        System.out.println("Cat start eat! Try it: " + appetite);
+        if (p.getFood() - appetite >= 0) {
+            p.decreaseFood(appetite);
+            isFull = true;
+            p.info();
+        } else {
+            System.out.println("Plate not have count food: " + appetite);
+            p.addFood(appetite);
+            isFull = true;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isFull() {
+        return isFull;
+    }
+}
